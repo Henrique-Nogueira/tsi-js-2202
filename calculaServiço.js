91f96dcec1faa0a55410ec7a08cde7251f7a2628
HENRIQUE NOGUEIRA DOS SANTOS <henrique.nsantos1@senacsp.edu.br>
2022-09-23 22:06:39 -0300

    adicionar elementos

--- calculaServiço.js
+++ calculaServiço.js
@@ -1,10 +1,21 @@
-/*
-Criar uma calculadora de valor a ser
-cobrado do cliente
-O valor do serviço pode variar conforme o
-prazo.
-Os valores e prazos devem ser armazenados
-em uma matriz.
-O cálculo deve ficar obrigatoriamente em
-uma função
-*/
+let serv = document.getElementById('servico');
+let prazo = document.getElementById('prazo');
+let res = document.getElementById('resultado');
+console.log(serv.value);
+
+
+let preco = CalcularPreco(serv.value, prazo.value);
+
+res.innerText = 'Funcionou!';
+
+ btnCalcular.addEventListener('click',function(evento) {
+
+});
+
+function CalcularPreco( servico, prazo) {
+
+let tabela = [['$$$','$$','$'],
+             ['$$$$', '$$$', '$$'], 
+             ['Não faz', 'Não faz', '$$$']];
+        return tabela[servico][prazo];
+}
